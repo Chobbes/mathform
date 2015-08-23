546ffd22fe7321391d241d5b12f2f1996f0dcde6
Calvin Beck <hobbes@ualberta.ca>
2015-08-23 13:25:53 -0600

    Start of parsers...

---
+++ src/Text/MathForm/Readers/Sage.hs
@@ -0,0 +1,58 @@
+{- Copyright (C) 2014 Calvin Beck
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation files
+   (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+-}
+
+module Text.MathForm.Readers.Sage where
+
+import Text.MathForm.MathTypes
+import Text.MathForm.Readers.SageTokens
+import Text.Parsec
+import Text.Parsec.Expr
+import Text.Parsec.Text
+import Control.Monad
+import Control.Monad.Identity
+import qualified Data.Text as T
+
+
+parseSage :: Parser MathForm
+parseSage = buildExpressionParser table term
+
+
+term :: Parser MathForm
+term = parens parseSage
+       <|> fmap (either IntLit FloatLit) naturalOrFloat
+       <|> liftM Symbol identifier
+
+
+-- | https://hackage.haskell.org/package/parsec-3.1.9/docs/Text-Parsec-Expr.html
+-- is pretty much exactly what we need here.
+table :: OperatorTable T.Text st Identity MathForm
+table = [ [binary "**" Pow AssocLeft, binary "^" Pow AssocLeft]
+        , [prefix "-" Neg, prefix "+" Pos]
+        , [binary "*" Mult AssocLeft, binary "/" Div AssocLeft]
+        , [binary "+" Plus AssocLeft, binary "-" Sub AssocLeft]
+        ]
+
+
+binary  name fun = Infix (do { reservedOp name; return fun })
+prefix  name fun = Prefix (do { reservedOp name; return fun })
+postfix name fun = Postfix (do { reservedOp name; return fun })
---
+++ src/Text/MathForm/Readers/SageTokens.hs
@@ -0,0 +1,117 @@
+{- Copyright (C) 2014 Calvin Beck
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation files
+   (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+-}
+
+
+module Text.MathForm.Readers.SageTokens where
+
+import Text.Parsec
+import Text.Parsec.Language
+import Text.Parsec.Token as T
+import Control.Monad.Identity
+import qualified Data.Text as Text
+
+identifier = T.identifier pyTokenParser
+reserved = T.reserved pyTokenParser
+operator = T.operator pyTokenParser
+reservedOp = T.reservedOp pyTokenParser
+charLiteral = T.charLiteral pyTokenParser
+stringLiteral = T.stringLiteral pyTokenParser
+natural = T.natural pyTokenParser
+integer = T.integer pyTokenParser
+float = T.float pyTokenParser
+naturalOrFloat = T.naturalOrFloat pyTokenParser
+decimal = T.decimal pyTokenParser
+hexadecimal = T.hexadecimal pyTokenParser
+octal = T.octal pyTokenParser
+symbol = T.symbol pyTokenParser
+lexeme = T.lexeme pyTokenParser
+whiteSpace = T.whiteSpace pyTokenParser
+parens = T.parens pyTokenParser
+braces = T.braces pyTokenParser
+angles = T.angles pyTokenParser
+brackets = T.brackets pyTokenParser
+squares = T.squares pyTokenParser
+semi = T.semi pyTokenParser
+comma = T.comma pyTokenParser
+colon = T.colon pyTokenParser
+dot = T.dot pyTokenParser
+semiSep = T.semiSep pyTokenParser
+semiSep1 = T.semiSep1 pyTokenParser
+commaSep = T.commaSep pyTokenParser
+commaSep1 = T.commaSep1 pyTokenParser
+
+
+pyTokenParser = makeTokenParser pythonDef
+
+pythonDef :: GenLanguageDef Text.Text st Identity
+pythonDef = LanguageDef
+            { commentStart = ""
+            , commentEnd = ""
+            , commentLine = "#"
+            , nestedComments = True
+            , identStart = letter <|> char '_'
+            , identLetter = alphaNum <|> char '_'
+            , opStart = oneOf ":!#$%&*+./<=>?@\\^|-~"
+            , opLetter = oneOf  ":!#$%&*+./<=>?@\\^|-~"
+            , reservedOpNames = [ "*"
+                                , "+"
+                                , "-"
+                                , "**"
+                                ]
+            , reservedNames = [ "False"
+                              , "class"
+                              , "finally"
+                              , "is"
+                              , "return"
+                              , "None"
+                              , "continue"
+                              , "for"
+                              , "lambda"
+                              , "try"
+                              , "True"
+                              , "def"
+                              , "from"
+                              , "nonlocal"
+                              , "while"
+                              , "and"
+                              , "del"
+                              , "global"
+                              , "not"
+                              , "with"
+                              , "as"
+                              , "elif"
+                              , "if"
+                              , "or"
+                              , "yield"
+                              , "assert"
+                              , "else"
+                              , "import"
+                              , "pass"
+                              , "break"
+                              , "except"
+                              , "in"
+                              , "raise"
+                              ]
+            , caseSensitive = True
+            }
+
